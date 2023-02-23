e95b484ec830c7c320904ac572800b7dbda08fe3
Lee <2634048793@qq.com>
2023-02-24 00:26:39 +0800

    snake over

--- src/实例/贪吃蛇/sanke.js
+++ src/实例/贪吃蛇/sanke.js
@@ -1,72 +1,158 @@
 (function () {
-    var container = document.getElementById("container");
-    for (var a = 0; a < 400; a++) {
-        var div = document.createElement("div");
-        div.style.width = "20px";
-        div.style.height = "20px";
-        div.style.backgroundColor = "grey";
-        container.appendChild(div);
-    }
-    var paragraphs = container.getElementsByTagName("div");
-    var map = [];
-    var sanke = [
-        [6, 0],
-        [5, 0],
-        [4, 0],
-        [3, 0],
-        [2, 0],
-    ];
-    var row = [];
-    for (var i = 0; i < paragraphs.length; i++) {
-        row === null || row === void 0 ? void 0 : row.push(paragraphs[i]);
-        if (row.length === 20) {
-            map.push(row);
-            row = [];
+    function createMap() {
+        var container = document.getElementById("container");
+        for (var a = 0; a < 400; a++) {
+            var div = document.createElement("div");
+            div.style.width = "20px";
+            div.style.height = "20px";
+            div.style.backgroundColor = "grey";
+            container.appendChild(div);
         }
-    }
-    for (var i = 0; i < sanke.length; i++) {
-        var x = sanke[i][0];
-        var y = sanke[i][1];
-        if (i === 0) {
-            map[y][x].style.backgroundColor = "green";
+        var paragraphs = container.getElementsByTagName("div");
+        var map = [];
+        var row = [];
+        for (var i = 0; i < paragraphs.length; i++) {
+            row === null || row === void 0 ? void 0 : row.push(paragraphs[i]);
+            if (row.length === 20) {
+                map.push(row);
+                row = [];
+            }
         }
-        else {
-            map[y][x].style.backgroundColor = "black";
-        }
+        return map;
     }
-    var randomFood = Math.floor(Math.random() * (400 - sanke.length));
-    var food = [0, 0];
-    for (var y = 0; y < 20; y++) {
-        for (var x = 0; x < 20; x++) {
-            var hasSnake = false;
-            for (var i = 0; i < sanke.length; i++) {
-                var sx = sanke[i][0];
-                var sy = sanke[i][1];
-                if (sx === x && sy === y) {
-                    hasSnake = true;
+    function createSnake() {
+        var sanke = [
+            [4, 0],
+            [3, 0],
+            [2, 0],
+            [1, 0],
+            [0, 0],
+        ];
+        return sanke;
+    }
+    function createFood() {
+        var randomFood = Math.floor(Math.random() * (400 - sanke.length));
+        var food = [0, 0];
+        for (var y = 0; y < 20; y++) {
+            for (var x = 0; x < 20; x++) {
+                var hasSnake = false;
+                for (var i = 0; i < sanke.length; i++) {
+                    var sx = sanke[i][0];
+                    var sy = sanke[i][1];
+                    if (sx === x && sy === y) {
+                        hasSnake = true;
+                    }
+                }
+                if (!hasSnake) {
+                    randomFood--;
+                }
+                if (randomFood === 0) {
+                    food[0] = x;
+                    food[1] = y;
                 }
             }
-            if (!hasSnake) {
-                randomFood--;
-            }
-            if (randomFood === 0) {
-                food[0] = x;
-                food[1] = y;
+        }
+        return food;
+    }
+    function renderMap() {
+        for (var y = 0; y < 20; y++) {
+            for (var x = 0; x < 20; x++) {
+                map[y][x].style.backgroundColor = "grey";
             }
         }
     }
-    var fx = food[0];
-    var fy = food[1];
-    map[fy][fx].style.backgroundColor = "pink";
-})();
-var empty = [20, 20];
-var count = 0;
-while (true) {
-    count++;
-    var x = Math.floor(Math.random() * 20);
-    var y = Math.floor(Math.random() * 20);
-    console.log({ count: count, x: x, y: y });
-    if (x === empty[0] && y === empty[1]) {
-        break;
+    function renderFood() {
+        var fx = food[0];
+        var fy = food[1];
+        map[fy][fx].style.backgroundColor = "pink";
     }
-}
+    function renderSnake() {
+        for (var i = 0; i < sanke.length; i++) {
+            var x = sanke[i][0];
+            var y = sanke[i][1];
+            if (i === 0) {
+                map[y][x].style.backgroundColor = "green";
+            }
+            else {
+                map[y][x].style.backgroundColor = "black";
+            }
+        }
+    }
+    function moveSnake() {
+        var last = sanke.pop();
+        var first = sanke[0];
+        last[0] = first[0] + direction[0];
+        last[1] = first[1] + direction[1];
+        sanke.unshift(last);
+    }
+    function eatFoodWhenCounter() {
+        var first = sanke[0];
+        var last = sanke[sanke.length - 1];
+        if (first[0] === food[0] && first[1] === food[1]) {
+            var added = [last[0], last[1]];
+            sanke.push(added);
+            food = createFood();
+        }
+    }
+    function dieWhenEatSelf() {
+        var xt = sanke[0][0];
+        var yt = sanke[0][1];
+        for (var a = 1; a < sanke.length; a++) {
+            var sx = sanke[a][0];
+            var xy = sanke[a][1];
+            if (xt === sx && yt === xy) {
+                gameOver();
+            }
+        }
+    }
+    function dieWhenOut() {
+        var first = sanke[0];
+        if (first[0] === 20 ||
+            first[0] === -1 ||
+            first[1] === 20 ||
+            first[1] === -1) {
+            gameOver();
+        }
+    }
+    function control() {
+        window.addEventListener("keydown", function (event) {
+            if (event.code === "ArrowRight" && direction[0] !== -1) {
+                direction[0] = 1;
+                direction[1] = 0;
+            }
+            if (event.code === "ArrowLeft" && direction[0] !== 1) {
+                direction[0] = -1;
+                direction[1] = 0;
+            }
+            if (event.code === "ArrowUp" && direction[1] !== 1) {
+                direction[0] = 0;
+                direction[1] = -1;
+            }
+            if (event.code === "ArrowDown" && direction[1] !== -1) {
+                direction[0] = 0;
+                direction[1] = 1;
+            }
+        });
+    }
+    function gameOver() {
+        alert("Game Over");
+        window.location.reload();
+    }
+    var map = createMap();
+    var sanke = createSnake();
+    var direction = [1, 0];
+    var food = createFood();
+    control();
+    renderMap();
+    renderSnake();
+    renderFood();
+    setInterval(function () {
+        moveSnake();
+        eatFoodWhenCounter();
+        dieWhenEatSelf();
+        dieWhenOut();
+        renderMap();
+        renderFood();
+        renderSnake();
+    }, 300);
+})();
--- src/实例/贪吃蛇/sanke.ts
+++ src/实例/贪吃蛇/sanke.ts
@@ -1,66 +1,172 @@
 (function () {
-  const container = document.getElementById("container") as HTMLDivElement;
-  for (let a = 0; a < 400; a++) {
-    const div = document.createElement("div");
-    div.style.width = "20px";
-    div.style.height = "20px";
-    div.style.backgroundColor = "grey";
-    container.appendChild(div);
-  }
-
-  const paragraphs = container.getElementsByTagName("div");
-  const map: HTMLDivElement[][] = [];
-  const sanke: [x: number, y: number][] = [
-    [6, 0],
-    [5, 0],
-    [4, 0],
-    [3, 0],
-    [2, 0],
-  ];
-
-  let row: HTMLDivElement[] = [];
-  for (let i = 0; i < paragraphs.length; i++) {
-    row?.push(paragraphs[i]);
-    if (row.length === 20) {
-      map.push(row);
-      row = [];
+  function createMap() {
+    const container = document.getElementById("container") as HTMLDivElement;
+    for (let a = 0; a < 400; a++) {
+      const div = document.createElement("div");
+      div.style.width = "20px";
+      div.style.height = "20px";
+      div.style.backgroundColor = "grey";
+      container.appendChild(div);
     }
-  }
+    const paragraphs = container.getElementsByTagName("div");
 
-  for (let i = 0; i < sanke.length; i++) {
-    const x = sanke[i][0];
-    const y = sanke[i][1];
-
-    if (i === 0) {
-      map[y][x].style.backgroundColor = "green";
-    } else {
-      map[y][x].style.backgroundColor = "black";
+    const map: HTMLDivElement[][] = [];
+    let row: HTMLDivElement[] = [];
+    for (let i = 0; i < paragraphs.length; i++) {
+      row?.push(paragraphs[i]);
+      if (row.length === 20) {
+        map.push(row);
+        row = [];
+      }
     }
-  }
-  let randomFood = Math.floor(Math.random() * (400 - sanke.length));
-  const food: [x: number, y: number] = [0, 0];
 
-  for (let y = 0; y < 20; y++) {
-    for (let x = 0; x < 20; x++) {
-      let hasSnake = false;
-      for (let i = 0; i < sanke.length; i++) {
-        const sx = sanke[i][0];
-        const sy = sanke[i][1];
-        if (sx === x && sy === y) {
-          hasSnake = true;
+    return map;
+  }
+  function createSnake() {
+    const sanke: [x: number, y: number][] = [
+      [4, 0],
+      [3, 0],
+      [2, 0],
+      [1, 0],
+      [0, 0],
+    ];
+    return sanke;
+  }
+  function createFood() {
+    let randomFood = Math.floor(Math.random() * (400 - sanke.length));
+    const food: [x: number, y: number] = [0, 0];
+
+    for (let y = 0; y < 20; y++) {
+      for (let x = 0; x < 20; x++) {
+        let hasSnake = false;
+        for (let i = 0; i < sanke.length; i++) {
+          const sx = sanke[i][0];
+          const sy = sanke[i][1];
+          if (sx === x && sy === y) {
+            hasSnake = true;
+          }
+        }
+        if (!hasSnake) {
+          randomFood--;
+        }
+        if (randomFood === 0) {
+          food[0] = x;
+          food[1] = y;
         }
       }
-      if (!hasSnake) {
-        randomFood--;
-      }
-      if (randomFood === 0) {
-        food[0] = x;
-        food[1] = y;
+    }
+
+    return food;
+  }
+  function renderMap() {
+    for (let y = 0; y < 20; y++) {
+      for (let x = 0; x < 20; x++) {
+        map[y][x].style.backgroundColor = "grey";
       }
     }
   }
+  function renderFood() {
+    const fx = food[0];
+    const fy = food[1];
+    map[fy][fx].style.backgroundColor = "pink";
+  }
+  function renderSnake() {
+    for (let i = 0; i < sanke.length; i++) {
+      const x = sanke[i][0];
+      const y = sanke[i][1];
 
-  const fx = food[0];
-  const fy = food[1];
-  map[fy][fx].style.backgroundColor = "pink";
+      if (i === 0) {
+        map[y][x].style.backgroundColor = "green";
+      } else {
+        map[y][x].style.backgroundColor = "black";
+      }
+    }
+  }
+  function moveSnake() {
+    const last = sanke.pop() as [x: number, y: number];
+    const first = sanke[0];
+
+    last[0] = first[0] + direction[0];
+    last[1] = first[1] + direction[1];
+
+    sanke.unshift(last);
+  }
+  function eatFoodWhenCounter() {
+    const first = sanke[0];
+    const last = sanke[sanke.length - 1];
+    if (first[0] === food[0] && first[1] === food[1]) {
+      const added = [last[0], last[1]];
+      sanke.push(added as never);
+      food = createFood();
+    }
+  }
+  function dieWhenEatSelf() {
+    const xt = sanke[0][0];
+    const yt = sanke[0][1];
+    for (let a = 1; a < sanke.length; a++) {
+      const sx = sanke[a][0];
+      const xy = sanke[a][1];
+      if (xt === sx && yt === xy) {
+        gameOver();
+      }
+    }
+  }
+  function dieWhenOut() {
+    const first = sanke[0];
+    if (
+      first[0] === 20 ||
+      first[0] === -1 ||
+      first[1] === 20 ||
+      first[1] === -1
+    ) {
+      gameOver();
+    }
+  }
+  function control() {
+    window.addEventListener("keydown", (event) => {
+      if (event.code === "ArrowRight" && direction[0] !== -1) {
+        direction[0] = 1;
+        direction[1] = 0;
+      }
+      if (event.code === "ArrowLeft" && direction[0] !== 1) {
+        direction[0] = -1;
+        direction[1] = 0;
+      }
+      if (event.code === "ArrowUp" && direction[1] !== 1) {
+        direction[0] = 0;
+        direction[1] = -1;
+      }
+      if (event.code === "ArrowDown" && direction[1] !== -1) {
+        direction[0] = 0;
+        direction[1] = 1;
+      }
+    });
+  }
+  function gameOver() {
+    alert("Game Over");
+    window.location.reload();
+  }
+
+  const map: HTMLDivElement[][] = createMap();
+  const sanke: [x: number, y: number][] = createSnake();
+  const direction = [1, 0];
+  let food = createFood();
+
+  control();
+
+  renderMap();
+  renderSnake();
+  renderFood();
+
+  setInterval(() => {
+    moveSnake();
+
+    eatFoodWhenCounter();
+    dieWhenEatSelf();
+    dieWhenOut();
+
+    renderMap();
+    renderFood();
+    renderSnake();
+  }, 300);
 })();
