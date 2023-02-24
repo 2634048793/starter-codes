121a18b1ed66455e6bcac2ec1080e9ec34d6f5e2
Lee <2634048793@qq.com>
2023-02-24 20:58:20 +0800

    over

--- src/实例/贪吃蛇/sanke.js
+++ src/实例/贪吃蛇/sanke.js
@@ -1,7 +1,8 @@
 (function () {
-    // 生成地图（20*20个盒子）
+    /** 生成地图（20*20个盒子） */
     function createMap() {
         var container = document.getElementById("container");
+        // 循环生成400个盒子
         for (var a = 0; a < 400; a++) {
             var div = document.createElement("div");
             div.style.width = "20px";
@@ -9,6 +10,8 @@
             div.style.backgroundColor = "grey";
             container.appendChild(div);
         }
+        // index = y * 20 + x;
+        // 将一维的400个元素转换为二维的20*20矩阵以方便读写，具体为：将每20个元素保存为一行，保存20行
         var paragraphs = container.getElementsByTagName("div");
         var map = [];
         var row = [];
@@ -21,7 +24,7 @@
         }
         return map;
     }
-    // 生成蛇的身体
+    /** 生成蛇的身体 */
     function createSnake() {
         var sanke = [
             [4, 0],
@@ -32,12 +35,15 @@
         ];
         return sanke;
     }
-    // 生成食物
+    /** 生成食物 */
     function createFood() {
+        // 食物可以存放的空位有(400-snake.lenght)个，随机选择一个 0 - (400-snake.length) 的数字，并在地图上找到对应的空位
         var randomFood = Math.floor(Math.random() * (400 - sanke.length));
         var food = [0, 0];
+        // 遍历地图
         for (var y = 0; y < 20; y++) {
             for (var x = 0; x < 20; x++) {
+                // 遍历蛇的身体，判断当前位置是否被蛇占据
                 var hasSnake = false;
                 for (var i = 0; i < sanke.length; i++) {
                     var sx = sanke[i][0];
@@ -46,9 +52,11 @@
                         hasSnake = true;
                     }
                 }
+                // 若未被占据则 randomFood - 1
                 if (!hasSnake) {
                     randomFood--;
                 }
+                // 当 randomFood 为零时表示找到了食物的位置
                 if (randomFood === 0) {
                     food[0] = x;
                     food[1] = y;
@@ -57,7 +65,7 @@
         }
         return food;
     }
-    // 重置地图
+    /** 重置地图 */
     function renderMap() {
         for (var y = 0; y < 20; y++) {
             for (var x = 0; x < 20; x++) {
@@ -65,44 +73,42 @@
             }
         }
     }
-    // 重置食物
+    /** 绘制食物 */
     function renderFood() {
         var fx = food[0];
         var fy = food[1];
         map[fy][fx].style.backgroundColor = "pink";
     }
-    // 重置蛇身体
+    /** 绘制蛇身体 */
     function renderSnake() {
-        for (var i = 0; i < sanke.length; i++) {
-            var x = sanke[i][0];
-            var y = sanke[i][1];
-            if (i === 0) {
-                map[y][x].style.backgroundColor = "green";
-            }
-            else {
-                map[y][x].style.backgroundColor = "black";
-            }
+        var _a = sanke[0], x = _a[0], y = _a[1];
+        map[y][x].style.backgroundColor = "green";
+        for (var i = 1; i < sanke.length; i++) {
+            var _b = sanke[i], x_1 = _b[0], y_1 = _b[1];
+            map[y_1][x_1].style.backgroundColor = "black";
         }
     }
-    // 监听蛇的移动
+    /** 移动蛇 */
     function moveSnake() {
+        // 将蛇朝着 direction 的方向移动：将尾巴取出来，并将坐标设置为蛇头下一步的位置，并放到数组的首位，即蛇头的位置
         var last = sanke.pop();
         var first = sanke[0];
         last[0] = first[0] + direction[0];
         last[1] = first[1] + direction[1];
         sanke.unshift(last);
     }
-    // 迟到食物
+    /** 判断是否吃食物 如果吃到食物则蛇的身体长度+1 并重新生成食物 */
     function eatFoodWhenCounter() {
         var first = sanke[0];
         var last = sanke[sanke.length - 1];
         if (first[0] === food[0] && first[1] === food[1]) {
+            // 复制蛇的尾巴坐标作为新增的身体
             var added = [last[0], last[1]];
             sanke.push(added);
             food = createFood();
         }
     }
-    // 吃到蛇身
+    /** 判断蛇头是否吃到蛇身 吃到则游戏结束 */
     function dieWhenEatSelf() {
         var xt = sanke[0][0];
         var yt = sanke[0][1];
@@ -114,7 +120,7 @@
             }
         }
     }
-    // 撞到墙
+    /** 判断蛇头是否出界 出界则游戏结束 */
     // function dieWhenOut() {
     //   const first = sanke[0];
     //   if (
@@ -126,7 +132,8 @@
     //     gameOver();
     //   }
     // }
-    function enter() {
+    /** 穿墙 蛇头出界则在另一端再次出现 */
+    function throughWalls() {
         var tx = sanke[0][0];
         var ty = sanke[0][1];
         if (tx === 20) {
@@ -142,7 +149,7 @@
             sanke[0][1] = 19;
         }
     }
-    // 使蛇不能回头
+    /** 监听键盘的事件 控制蛇头的方向并且不能反方向走 */
     function control() {
         window.addEventListener("keydown", function (event) {
             if (event.code === "ArrowRight" && direction[0] !== -1) {
@@ -181,7 +188,7 @@
         eatFoodWhenCounter();
         dieWhenEatSelf();
         // dieWhenOut();
-        enter();
+        throughWalls();
         renderMap();
         renderFood();
         renderSnake();
--- src/实例/贪吃蛇/sanke.ts
+++ src/实例/贪吃蛇/sanke.ts
@@ -1,7 +1,8 @@
 (function () {
-  // 生成地图（20*20个盒子）
+  /** 生成地图（20*20个盒子） */
   function createMap() {
     const container = document.getElementById("container") as HTMLDivElement;
+    // 循环生成400个盒子
     for (let a = 0; a < 400; a++) {
       const div = document.createElement("div");
       div.style.width = "20px";
@@ -9,8 +10,10 @@
       div.style.backgroundColor = "grey";
       container.appendChild(div);
     }
-    const paragraphs = container.getElementsByTagName("div");
 
+    // index = y * 20 + x;
+    // 将一维的400个元素转换为二维的20*20矩阵以方便读写，具体为：将每20个元素保存为一行，保存20行
+    const paragraphs = container.getElementsByTagName("div");
     const map: HTMLDivElement[][] = [];
     let row: HTMLDivElement[] = [];
     for (let i = 0; i < paragraphs.length; i++) {
@@ -23,7 +26,7 @@
 
     return map;
   }
-  // 生成蛇的身体
+  /** 生成蛇的身体 */
   function createSnake() {
     const sanke: [x: number, y: number][] = [
       [4, 0],
@@ -34,13 +37,16 @@
     ];
     return sanke;
   }
-  // 生成食物
+  /** 生成食物 */
   function createFood() {
+    // 食物可以存放的空位有(400-snake.lenght)个，随机选择一个 0 - (400-snake.length) 的数字，并在地图上找到对应的空位
     let randomFood = Math.floor(Math.random() * (400 - sanke.length));
     const food: [x: number, y: number] = [0, 0];
 
+    // 遍历地图
     for (let y = 0; y < 20; y++) {
       for (let x = 0; x < 20; x++) {
+        // 遍历蛇的身体，判断当前位置是否被蛇占据
         let hasSnake = false;
         for (let i = 0; i < sanke.length; i++) {
           const sx = sanke[i][0];
@@ -49,9 +55,11 @@
             hasSnake = true;
           }
         }
+        // 若未被占据则 randomFood - 1
         if (!hasSnake) {
           randomFood--;
         }
+        // 当 randomFood 为零时表示找到了食物的位置
         if (randomFood === 0) {
           food[0] = x;
           food[1] = y;
@@ -61,7 +69,7 @@
 
     return food;
   }
-  // 重置地图
+  /** 重置地图 */
   function renderMap() {
     for (let y = 0; y < 20; y++) {
       for (let x = 0; x < 20; x++) {
@@ -69,27 +77,25 @@
       }
     }
   }
-  // 重置食物
+  /** 绘制食物 */
   function renderFood() {
     const fx = food[0];
     const fy = food[1];
     map[fy][fx].style.backgroundColor = "pink";
   }
-  // 重置蛇身体
+  /** 绘制蛇身体 */
   function renderSnake() {
-    for (let i = 0; i < sanke.length; i++) {
-      const x = sanke[i][0];
-      const y = sanke[i][1];
+    const [x, y] = sanke[0];
+    map[y][x].style.backgroundColor = "green";
 
-      if (i === 0) {
-        map[y][x].style.backgroundColor = "green";
-      } else {
-        map[y][x].style.backgroundColor = "black";
-      }
+    for (let i = 1; i < sanke.length; i++) {
+      const [x, y] = sanke[i];
+      map[y][x].style.backgroundColor = "black";
     }
   }
-  // 监听蛇的移动
+  /** 移动蛇 */
   function moveSnake() {
+    // 将蛇朝着 direction 的方向移动：将尾巴取出来，并将坐标设置为蛇头下一步的位置，并放到数组的首位，即蛇头的位置
     const last = sanke.pop() as [x: number, y: number];
     const first = sanke[0];
 
@@ -98,17 +104,18 @@
 
     sanke.unshift(last);
   }
-  // 迟到食物
+  /** 判断是否吃食物 如果吃到食物则蛇的身体长度+1 并重新生成食物 */
   function eatFoodWhenCounter() {
     const first = sanke[0];
     const last = sanke[sanke.length - 1];
     if (first[0] === food[0] && first[1] === food[1]) {
+      // 复制蛇的尾巴坐标作为新增的身体
       const added = [last[0], last[1]];
       sanke.push(added as never);
       food = createFood();
     }
   }
-  // 吃到蛇身
+  /** 判断蛇头是否吃到蛇身 吃到则游戏结束 */
   function dieWhenEatSelf() {
     const xt = sanke[0][0];
     const yt = sanke[0][1];
@@ -120,7 +127,7 @@
       }
     }
   }
-  // 撞到墙
+  /** 判断蛇头是否出界 出界则游戏结束 */
   // function dieWhenOut() {
   //   const first = sanke[0];
   //   if (
@@ -132,8 +139,8 @@
   //     gameOver();
   //   }
   // }
-
-  function enter() {
+  /** 穿墙 蛇头出界则在另一端再次出现 */ 
+  function throughWalls() {
     const tx = sanke[0][0];
     const ty = sanke[0][1];
     if (tx === 20) {
@@ -149,7 +156,7 @@
       sanke[0][1] = 19;
     }
   }
-  // 使蛇不能回头
+  /** 监听键盘的事件 控制蛇头的方向并且不能反方向走 */ 
   function control() {
     window.addEventListener("keydown", (event) => {
       if (event.code === "ArrowRight" && direction[0] !== -1) {
@@ -193,7 +200,7 @@
     eatFoodWhenCounter();
     dieWhenEatSelf();
     // dieWhenOut();
-    enter();
+    throughWalls();
 
     renderMap();
     renderFood();
